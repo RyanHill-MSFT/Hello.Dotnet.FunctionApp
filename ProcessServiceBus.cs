26b9fd4a5b7fbbdc517a2e992145477c29e88b62
Ryan Hill <ryanchill@hotmail.com>
2024-07-09 11:15:34 -0400

    add service bus trigger

---
+++ ProcessServiceBus.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+using DnsClient.Internal;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Hello.Dotnet.FunctionApp
+{
+    public class ProcessServiceBus
+    {
+        private readonly ILogger<ProcessServiceBus> _logger;
+
+        public ProcessServiceBus(ILogger<ProcessServiceBus> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(ProcessServiceBus))]
+        public async Task Run(
+            [ServiceBusTrigger("my-queue", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message,
+            ServiceBusMessageActions messageActions)
+        {
+            _logger.LogInformation("ID: {id}\r\nBody: {body}\r\nContent-Type: {contentType}", message.MessageId, message.Body, message.ContentType);
+
+            await messageActions.CompleteMessageAsync(message);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,17 +2,34 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 
-var host = new HostBuilder()
+new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        var managedIdentityId = context.Configuration["ManagedIdentityId"];
+        var tokenCredential = string.IsNullOrEmpty(managedIdentityId)
+        ? new DefaultAzureCredential()
+        : new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = managedIdentityId });
+
+        builder.SetBasePath(context.HostingEnvironment.ContentRootPath)
+               .AddEnvironmentVariables()/*
+               .AddAzureAppConfiguration(options =>
+               {
+                   options.Connect(new Uri(context.Configuration["AppConfigurationEndpoint"].ToString()), tokenCredential);
+               })*/;
+    })
     .ConfigureLogging((builder, logging) =>
     {
         logging.AddConsole();
     })
-    .ConfigureServices(services => {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
+    .ConfigureServices(services =>
+    {
+        services.AddLogging()
+                .AddApplicationInsightsTelemetryWorkerService()
+                .ConfigureFunctionsApplicationInsights();
     })
-    .Build();
-
-host.Run();
+    .Build()
+    .Run();
